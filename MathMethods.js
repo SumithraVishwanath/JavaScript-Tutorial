76fb8ab78876a9f8cefb586c22e1a8e8c483d7d6
SumithraVishwanath <sumithravish@gmail.com>
2021-06-21 02:33:47 -0700

    Different types of Math functions

---
+++ MathMethods.js
@@ -0,0 +1,27 @@
+let maxValue = Math.max(6,48,2,4,65,5,25,24,544)
+console.log(maxValue);   //544
+
+let minValue = Math.min(5,8,9,4,75,464,5,7)
+console.log(minValue);    //4
+
+let roundValue = Math.round(5.1)   
+let roundValue1 = Math.round(5.5)
+let roundValue2 = Math.round(5.8)
+console.log(roundValue);   //5
+console.log(roundValue1);  //6
+console.log(roundValue2);  //6
+
+let ceilValue = Math.ceil(6.1) 
+let ceilValue1 = Math.ceil(6.8)
+console.log(ceilValue);  //7
+console.log(ceilValue1); //7
+
+let floorValue = Math.floor(7.1)
+let floorValue1 = Math.floor(7.8)
+console.log(floorValue); //7
+console.log(floorValue1); //7
+
+// random numbers
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
