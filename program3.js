135f810e9c283f3be9998e9931e3e32ba7bd1fc8
SumithraVishwanath <sumithravish@gmail.com>
2021-06-22 00:40:17 -0700

    Program to replace the charcter with next character in alphabets

---
+++ program3.js
@@ -0,0 +1,7 @@
+/* Program to replace every character in a given string with the character following it in the alphabet */
+
+const moveCharsForward = (str) =>
+ str.split('').map(char => String.fromCharCode(char.charCodeAt(0)+ 1)).join('');
+
+console.log(moveCharsForward('abcd')); // bcde
+console.log(moveCharsForward('mnop'));  // nopq
